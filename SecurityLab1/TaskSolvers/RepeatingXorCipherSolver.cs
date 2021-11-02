a7ac69419593e38b450e5a3982b019cb717cb138
Artem Supryhan <suprigan.artem1@gmail.com>
2021-11-02 22:47:40 +0200

    Add Key length

--- SecurityLab1/TaskSolvers/RepeatingXorCipherSolver.cs
+++ SecurityLab1/TaskSolvers/RepeatingXorCipherSolver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SecurityLab1.Interfaces;
@@ -14,28 +15,74 @@ namespace SecurityLab1.TaskSolvers
             var text = await File.ReadAllTextAsync(filepath);
             var encodedText = Convert.FromBase64String(text);
             var coincidenceIndexes = CalculateIndexOfCoincidence(encodedText);
-            foreach (var pair in coincidenceIndexes)
-            {
-                Console.WriteLine(pair.Key + " " + pair.Value);
-            }
+            var sortedIndexes = coincidenceIndexes.OrderByDescending(x => x.Value);
+            // foreach (var pair in sortedIndexes)
+            // {
+            //     Console.WriteLine(pair.Key + " " + pair.Value);
+            // }
+            var keys = GetKeys(coincidenceIndexes);
+            var key = keys.First();
+           
         }
 
         private Dictionary<int, double> CalculateIndexOfCoincidence(byte[] text)
         {
+            var buffer = new List<byte>();
+            buffer.AddRange(text);
             var result = new Dictionary<int, double>();
             var coincidenceCount = 0d;
-            var textString = Encoding.UTF8.GetString(text);
-            for (int i = 1; i < textString.Length; i++)
+            for (int i = 0; i < text.Length; i++)
             {
-                for (int j = 0; j < textString.Length - i; j++)
+                var ch = buffer.Last();
+                buffer.RemoveAt(buffer.Count - 1);
+                buffer.Insert(0, ch);
+                for (int j = 0; j < text.Length - 1; j++)
                 {
-                    if (textString[j] == textString[j + i])
+                    if (text[j] == buffer[j])
                     {
                         coincidenceCount++;
                     }
                 }
-                result.Add(i, coincidenceCount / text.Length);
+                result.Add(i + 1, coincidenceCount / text.Length);
                 coincidenceCount = 0d;
+            }
+            // for (int i = 1; i < textString.Length; i++)
+            // {
+            //     for (int j = 0; j < textString.Length - i; j++)
+            //     {
+            //         if (textString[j] == textString[j + i])
+            //         {
+            //             coincidenceCount++;
+            //         }
+            //     }
+            //     result.Add(i, coincidenceCount / text.Length);
+            //     coincidenceCount = 0d;
+            // }
+
+            return result;
+        }
+
+        private List<int> GetKeys(Dictionary<int, double> coincidences)
+        {
+            var total = 0d;
+            var result = new List<int>();
+            foreach (var coincidence in coincidences)
+            {
+                for (int i = 1; i < coincidences.Count; i++)
+                {
+                    if (i % coincidence.Key == 0)
+                    {
+                        total += coincidence.Value;
+                    }
+                }
+                
+                var average = total / ((double)coincidences.Count / coincidence.Key);
+                if (average >= 0.06)
+                {
+                    result.Add(coincidence.Key);
+                }
+
+                total = 0d;
             }
 
             return result;
